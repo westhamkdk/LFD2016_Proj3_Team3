6706876b67cc146222815ec7309fd63765c88ac8
Do-Kyun <vamos.kim@gmail.com>
2016-05-22 08:24:01 -0700

    changed return type to nd array

--- project3/src/change.py
+++ project3/src/change.py
@@ -21,7 +21,7 @@ def make_reward_table(epi_num):
     for b in range(epi['reward'].size):
         reward.append(epi.values[b][5])
 
-    return reward
+    return np.array(reward)
 
 def make_pos_table(epi_num):  # 0~14
     pos = []
@@ -31,7 +31,7 @@ def make_pos_table(epi_num):  # 0~14
         temp = epi.values[a][4]
         cal = cal_action_num(temp)
         pos.append(cal)
-    return pos
+    return np.array(pos)
 
 def make_kibo_panel(epi_num):
 
@@ -42,6 +42,7 @@ def make_kibo_panel(epi_num):
     matrix = [[[0 for col in range(15)] for row in range(15)] for dim in range(epi['action'].size)]
     temp = [[0 for col in range(15)] for row in range(15)]
 
+    full_mat = []
     for a in range(epi['action'].size):
 
         cal = epi.values[a][4]
@@ -53,9 +54,11 @@ def make_kibo_panel(epi_num):
 
         df = pd.DataFrame(temp)
         new_dict[a] = df
+        full_mat.append(temp)
 
     panel = pd.Panel(data=new_dict)
-    return panel
+    print np.array(full_mat)
+    return np.array(full_mat)
 
 def get_kibo_pos_value(epi_num):
     return make_kibo_panel(epi_num), make_pos_table(epi_num), make_reward_table(epi_num)
@@ -66,7 +69,8 @@ if __name__ == '__main__':
 
     # episodes' index are sequential
     try:
-        with open('../data/episodes1.p', 'rb') as handle:
+        with open('../data/episodes.p', 'rb') as handle:
+            print "pickle loaded"
             episodes = pickle.load(handle)
     except:
         print "failed to load episodes"
