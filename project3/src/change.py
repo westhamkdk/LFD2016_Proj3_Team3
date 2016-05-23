2c6a3b50078a13a9e6572123cf33a287b2438cfe
dlstnry <dlstnry123@gmail.com>
2016-05-24 08:11:03 +0900

    saving

--- project3/src/change.py
+++ project3/src/change.py
@@ -53,7 +53,7 @@ class DataLoader(object):
                     self.preload_kibo, self.preload_pos, self.preload_reward = self.get_kibo_pos_value(eposide_id)
                 else:
                     kibo, pos, reward = self.get_kibo_pos_value(eposide_id)
-                    if eposide_id // 50 == 0:
+                    if eposide_id % 50 == 0:
                         print "episodo id : ", str(eposide_id), "/", str(self.episode_size)
                     try:
                         self.preload_kibo = np.concatenate((self.preload_kibo, kibo))
@@ -75,9 +75,16 @@ class DataLoader(object):
             self.preload_reward = np.reshape(self.preload_reward, (self.preload_reward.shape[0], 1))
 
             assert len(self.preload_kibo) == len(self.preload_pos) == len(self.preload_reward)
-            np.save('../data/kibo', self.preload_kibo)
-            np.save('../data/pos', self.preload_pos)
-            np.save('../data/reward',self.preload_reward)
+            if (episode_id != 0 and episode_id % 10000 == 0) or (episode_id ==(self.episode_size-1)):
+                print "saving..."
+                idx = episode_id // 10000
+                if episode_id == (self.episode_size-1):
+                    idx += 1
+                np.save('../data/kibo_%d' %idx, self.preload_kibo)
+                np.save('../data/pos_%d' %idx, self.preload_pos)
+                np.save('../data/reward_%d' %idx, self.preload_reward)
+                self.preload_kibo, self.preload_pos, self.preload_reward = None, None, None
+                print "saving complete"
 
     def generate_batch(self, batch_size):
         epoch_over = False
