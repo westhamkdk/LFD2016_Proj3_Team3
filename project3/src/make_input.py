56aa5a9c041f3a7e68b2e5446fb27ea3a3f436be
SookyoIn <dlstnry123@gmail.com>
2016-05-24 11:40:25 +0900

    push

--- project3/src/make_input.py
+++ project3/src/make_input.py
@@ -18,9 +18,9 @@ class DataLoader(object):
         self.preload_episode_size = self.episode_size
         self.current_episode_id = 0
         self.data_idx = 0
+        self.file_idx = file_idx
         self.preload_dataset()
         self.model_type = model_type
-        self.file_idx = file_idx
         print self.episode_size
 
     def load_episodes(self):
@@ -88,6 +88,59 @@ class DataLoader(object):
                     np.save('../data/reward_%d' %idx, self.preload_reward)
                     self.preload_kibo, self.preload_pos, self.preload_reward = None, None, None
                     print "saving complete"
+
+
+    def cal_action_num(self, temp):
+        cal = temp[0] * 15 + temp[1]
+        return cal
+
+
+    def make_reward_table(self, epi_num):
+        epi = self.episodes[epi_num]
+        reward = []
+
+        for b in range(epi['reward'].size - 1):
+            reward.append(epi.values[b][5])
+
+        return np.array(reward)
+
+
+    def make_pos_table(self, epi_num):  # 0~14
+        pos = []
+        epi = self.episodes[epi_num]
+
+        for a in range(epi['action'].size - 1):
+            temp = epi.values[a + 1][4]
+            cal = self.cal_action_num(temp)
+            pos.append(cal)
+        return np.array(pos)
+
+
+    def make_kibo_panel(self, epi_num):
+        epi = self.episodes[epi_num]
+
+        new_dict = dict()
+        temp = [[0 for col in range(15)] for row in range(15)]
+
+        full_mat = []
+        for a in range(epi['action'].size - 1):
+
+            cal = epi.values[a][4]
+
+            if a % 2 == 0:
+                temp[cal[0]][cal[1]] = 1
+            elif a % 2 == 1:
+                temp[cal[0]][cal[1]] = -1
+
+            df = pd.DataFrame(temp)
+            new_dict[a] = df
+            full_mat.append(copy.deepcopy(temp))
+
+        return np.array(full_mat)
+
+
+    def get_kibo_pos_value(self, epi_num):
+        return self.make_kibo_panel(epi_num), self.make_pos_table(epi_num), self.make_reward_table(epi_num)
 
 if __name__ == '__main__':
     data_loader = DataLoader(model_type="classification", file_idx=9)
